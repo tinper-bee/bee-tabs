146be70bdef782b23e266330fa0e83c48ac6dfc1
BoyuZhou <386607913@qq.com>
2017-03-14 23:36:09 +0800

    change style

---
+++ demo/demolist/Demo4.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * @title 基础tab
+ * @description
+ *
+ */
+
+ var callback = function(key){
+
+ }
+
+
+class Demo4 extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        return (
+            <Tabs
+      defaultActiveKey="2"
+      tabBarStyle ="fade"
+      onChange={callback}
+      renderTabBar={()=><ScrollableInkTabBar />}
+      renderTabContent={()=><TabContent />}
+    >
+      <TabPane tab='tab 1' key="1">first</TabPane>
+      <TabPane tab='tab 2' key="2">second</TabPane>
+      <TabPane tab='tab 3' key="3">third</TabPane>
+    </Tabs>
+        )
+    }
+}
