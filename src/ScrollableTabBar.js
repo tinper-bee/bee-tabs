adbfa140faf6d3199c58b24379f5586c758fcd82
jonymrshi <jonyshi@126.com>
2017-10-20 22:37:36 +0800

    ver. 1.0.3

--- src/ScrollableTabBar.js
+++ src/ScrollableTabBar.js
@@ -3,15 +3,8 @@ import ScrollableTabBarMixin from './ScrollableTabBarMixin';
 import TabBarMixin from './TabBarMixin';
 import createClass from 'create-react-class';
 
-<<<<<<< HEAD
-class ScrollableTabBar extends Component{
-
-  mixins: [TabBarMixin, ScrollableTabBarMixin]
-  
-=======
 const ScrollableTabBar = createClass({
   mixins: [TabBarMixin, ScrollableTabBarMixin],
->>>>>>> daily/1.0.2
   render() {
     const inkBarNode = this.getInkBarNode();
     const tabs = this.getTabs();
@@ -19,6 +12,6 @@ const ScrollableTabBar = createClass({
     return this.getRootNode(scrollbarNode);
   }
 
-}
+});
 
 export default ScrollableTabBar;
