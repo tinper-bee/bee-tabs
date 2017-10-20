44dfb2142f82dd547292dac6996f468e45a4bbc4
jonymrshi <jonyshi@126.com>
2017-10-20 22:36:15 +0800

    react 16升级

--- src/InkTabBar.js
+++ src/InkTabBar.js
@@ -3,21 +3,14 @@ import InkTabBarMixin from './InkTabBarMixin';
 import TabBarMixin from './TabBarMixin';
 import createClass from 'create-react-class';
 
-<<<<<<< HEAD
-class InkTabBar extends Component{
-
-  mixins: [TabBarMixin, InkTabBarMixin]
-
-=======
 const InkTabBar = createClass({
-  mixins: [TabBarMixin, InkTabBarMixin],
->>>>>>> daily/1.0.2
+  mixins: [TabBarMixin, InkTabBarMixin], 
   render() {
     const inkBarNode = this.getInkBarNode();
     const tabs = this.getTabs();
     return this.getRootNode([inkBarNode, tabs]);
   }
-
-}
+  
+});
 
 export default InkTabBar;
